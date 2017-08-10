a006202529a2f7182aaf94b791d7211c5ec01f7b
lihlepaie <pasielihle@gmail.com>
2017-08-10 17:48:32 +0200

    add.handlebars

--- server.js
+++ server.js
@@ -1,53 +1,79 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+// const nameRoutes = NameRoutes();
 var app = express();
-var greetedUser=[];
+var greetedUser = [];
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+})
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+
+app.get('/names/add', function(req, res) {
+    res.render('names/add')
+})
+
+app.post('/names/add', function(req, res){
+  res.render('names/add')
+})
+
 
 // create a route
 
-app.get('/greetings/:name', function (req, res) {
-  console.log(req.params.name);
-  var name = req.params.name;
-  greetedUser.push(name);
- res.send("Hello , "+req.params.name);
+app.get('/greetings/:name', function(req, res) {
+    console.log(req.params.name);
+    var name = req.params.name;
+    greetedUser.push(name);
+    res.send("Hello , " + req.params.name);
 
 });
 //create a greetedUser.push(name); route
-app.get('/greeted', function (req, res) {
-  console.log(greetedUser);
-  // var name = req.params.name;
-  var uniqueList=[];
-  for (var i = 0; i < greetedUser.length; i++) {
-    if(uniqueList.indexOf(greetedUser[i])===-1){
-      console.log(greetedUser);
-      uniqueList.push(greetedUser[i]);
+app.get('/greeted', function(req, res) {
+    console.log(greetedUser);
+    // var name = req.params.name;
+    var uniqueList = [];
+    for (var i = 0; i < greetedUser.length; i++) {
+        if (uniqueList.indexOf(greetedUser[i]) === -1) {
+            console.log(greetedUser);
+            uniqueList.push(greetedUser[i]);
+        }
     }
-  }
-    res.render('index', {names:uniqueList});
+    res.render('names/index', {
+        names: uniqueList
+    });
 
 });
 // creating a router for counter
-app.get('/counter/:names', function (req, res) {
-  var name = req.params.names;
-  var greetingsCounter=0;
-  for (var i = 0; i < greetedUser.length; i++) {
-  if ( greetedUser[i]=== name){
+app.get('/counter/:names', function(req, res) {
+    var name = req.params.names;
+    var greetingsCounter = 0;
+    for (var i = 0; i < greetedUser.length; i++) {
+        if (greetedUser[i] === name) {
 
-    greetingsCounter++;
-}
-}
-res.send("Hello, " + name + " has been greeted " +  greetingsCounter +  ' times ');
+            greetingsCounter++;
+        }
+    }
+    res.send("Hello, " + name + " has been greeted " + greetingsCounter + ' times ');
 });
 
 //start the server
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
- var host = server.address().address;
- var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
- console.log('node server.js', host, port);
+    console.log('node server.js', host, port);
 
 });
