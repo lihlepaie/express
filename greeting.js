e046c9f14735c1ed9d989ed0238278c1fbc359e3
lihlepaie <pasielihle@gmail.com>
2017-09-08 13:02:49 +0200

    update cli

--- greeting.js
+++ greeting.js
@@ -4,7 +4,7 @@ module.exports = function(models) {
     var language = []
 
     const index = function(req, res, next) {
-      res.render('names/add');
+        res.render('names/add');
 
     };
 
@@ -18,53 +18,45 @@ module.exports = function(models) {
         }
 
         if (!user.name || !language) {
-          req.flash('error', 'Name should not be blank and plase select language to be greeted in');
-          res.redirect('/greeted');
-        }
-        else {
-          models.Greeted.create( user, function(err, results){
-            if (err){
-              if (err.code === 11000){
-                models.Greeted.findOne({
-                  name : user.name
-                })
-                .exec(function(err, results){
-                  if (results){
-                    results.counter = results.counter + 1;
-                    results.save();
-                  }
-                })
-                req.flash('error', 'Welcome back, ' + user.name);
-              }
-              else {
-                return next(err);
-              }
-            }
-
-            models.Greeted.find({}, function(err, results){
+            req.flash('error', 'Name should not be blank and please select language to be greeted in');
+            res.redirect('/greetings');
+        } else {
+            models.Greeted.create(user, function(err, results) {
                 if (err) {
-                  return next(err);
-
-                  models.Greeted.remove({}, function(err, remove){
-                    if (err) {
-                      return err;
-                  
+                    if (err.code === 11000) {
+                        models.Greeted.findOne({
+                                name: user.name
+                            })
+                            .exec(function(err, results) {
+                                if (results) {
+                                    results.counter = results.counter + 1;
+                                    results.save();
+                                }
+                            })
+                        req.flash('error', 'Welcome back');
+                    } else {
+                        return next(err);
                     }
-
-                  })
                 }
-                else {
 
-                  var message = language + ", " + user.name;
-                  res.render('names/add', {
-                    message : message,
-                    count: results.length});
+                models.Greeted.find({}, function(err, results) {
+                    if (err) {
+                        return next(err);
 
-                }
+
+                    } else {
+
+                        var message = language + ", " + user.name;
+                        res.render('names/add', {
+                            message: message,
+                            count: results.length
+                        });
+
+                    }
+                })
+
             })
-
-          })
-          }
+        }
     }
 
 
@@ -74,10 +66,10 @@ module.exports = function(models) {
                 return next(err);
             }
             //else {
-                res.render('names/greeted', {
-                    names:names
+            res.render('names/greeted', {
+                names: names
 
-                });
+            });
             //}
         })
 
@@ -92,7 +84,9 @@ module.exports = function(models) {
         //         greetingsCounter++;
 
 
-        models.Greeted.findOne({ name: req.params.name}, function(err, greeted) {
+        models.Greeted.findOne({
+            name: req.params.name
+        }, function(err, greeted) {
 
             if (err) {
                 return next(err)
@@ -101,47 +95,32 @@ module.exports = function(models) {
             var mssg = "Hello, " + greeted.name + " has been greeted " + greeted.counter + ' times ';
             res.render("names/greetedCounter", {
                 name: mssg,
-
-
             });
 
-
-
-        });
-        // }
-        // }
-    }
-
-    const greetmsg = function(req, res) {
-        // console.log(req.body.name);
-        var name = req.params.name;
-        greetedUser.push(name);
-        res.send("Hello , " + req.params.name);
-    }
-
-    const counter = function(req, res) {
-        // console.log(greetedUser);
-        // var name = req.params.name;
-        // var uniqueList = [];
-        // for (var i = 0; i < greetedUser.length; i++) {
-        //     if (uniqueList.indexOf(greetedUser[i]) === -1) {
-        //         console.log(greetedUser);
-        //         uniqueList.push(greetedUser[i]);
-        //     }
-        // }
-
-
-        res.render('names/index', {
-            names: uniqueList,
-            number: counter
         });
 
     }
+
+    const reset = function(req, res, next) {
+
+        models.Greeted.remove({}, function(err, remove) {
+            if (err) {
+                return next(err);
+
+            }
+            res.redirect('/greetings');
+        })
+
+
+    }
+
+
     return {
         index,
         greet,
         greeted,
-        greetedCounter
+        greetedCounter,
+        reset
 
 
     }
--- server.js
+++ server.js
@@ -26,20 +26,13 @@ app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 * 30 }}));
 app.use(flash());
 
-
-app.get('/',function(req,res){
-  res.redirect('/greeted')
-})
-
+//routes
 app.get('/greetings', greetRoutes.index);
 app.post('/greetings', greetRoutes.greet);
 app.get('/greeted', greetRoutes.greeted);
-app.post('/names/add', greetRoutes.greet);
-app.get('/names/:greetings', greetRoutes.greeted);
 app.get('/counter/:name', greetRoutes.greetedCounter);
-app.post('/reset', function(req, res){
+app.get('/reset', greetRoutes.reset);
 
-});
 
 //start the server
 app.set('port',(process.env.PORT || 5000) );
